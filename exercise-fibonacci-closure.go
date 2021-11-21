410c27a5d87851da546cf43fa7c038935a5680e6
goodje <1475199+goodje@users.noreply.github.com>
2021-11-21 17:56:25 +0800

    exercise-fibonacci-closure.go

---
+++ exercise-fibonacci-closure.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	i := 0
+	a := 0
+	b := 1
+	d := func() {
+		i = i + 1
+	}
+	return func() int {
+		defer d()
+		if i == 0 {
+			return a
+		} else if i == 1 {
+			return b
+		}
+
+		v := a + b
+		a = b
+		b = v
+		return v
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
